1631ac0f5893fc86ad692128388c54def2314226
Thomas Lee <thomas.y.lee@protonmail.com>
2020-05-14 00:32:04 +0100

    Add AdamBackpropagation
    * AdamBackpropagation is a stochastic backpropagation enhanced by the
    Adam algorithm.

---
+++ src/main/kotlin/kotml/regression/optimization/backpropagation/AdamBackpropagation.kt
@@ -0,0 +1,119 @@
+package kotml.regression.optimization.backpropagation
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotml.extensions.* // ktlint-disable no-wildcard-imports
+import kotml.math.MutableVector
+import kotml.math.Vector
+import kotml.regression.Weights
+import kotml.regression.cost.CostFunction
+import kotml.regression.neural.FeedforwardNeuralNetwork
+import kotml.regression.optimization.IterativeOptimizer
+
+class AdamBackpropagation(
+    val network: FeedforwardNeuralNetwork,
+    val costFunction: CostFunction,
+    val betaM: Double = 0.99,
+    val betaV: Double = 0.999,
+    val epsilon: Double = 0.0001
+) : IterativeOptimizer<FeedforwardNeuralNetwork>(
+    regressorCount = network.layers.first().neurons.first().weights.coeffs.shape[0],
+    outputCount = network.layers.last().neurons.size,
+    model = network
+) {
+    /**
+     * Running average of the gradients.
+     */
+    private val m: List<List<Weights>> = network.layers.map { layer ->
+        layer.neurons.map { neuron ->
+            val constant: Double? = if (neuron.weights.hasConstant) 0.0 else null
+            Weights(constant, MutableVector.zeros(neuron.weights.coeffs.shape[0]))
+        }
+    }
+
+    /**
+     * Stores betaV raised to the power of the number of observations.
+     */
+    private var betaMProduct: Double = 1.0
+
+    /**
+     * Second moments of the gradients.
+     */
+    private val v: List<List<Weights>> = network.layers.map { layer ->
+        layer.neurons.map { neuron ->
+            val constant: Double? = if (neuron.weights.hasConstant) 0.0 else null
+            Weights(constant, MutableVector.zeros(neuron.weights.coeffs.shape[0]))
+        }
+    }
+
+    /**
+     * Stores betaV raised to the power of the number of observations.
+     */
+    private var betaVProduct: Double = 1.0
+
+    protected override fun addObservation(regressors: Vector, targets: Vector) {
+        betaMProduct *= betaM
+        betaVProduct *= betaV
+
+        val inputs = mutableListOf<Vector>()
+        val outputs = mutableListOf<Vector>()
+        network.layers.forEachIndexed { layerIndex, layer ->
+            val input: Vector =
+                if (layerIndex == 0)
+                    regressors
+                else
+                    outputs[layerIndex - 1]
+            inputs.add(input)
+            outputs.add(layer.evaluate(input))
+        }
+
+        val costDeriv: Vector = costFunction.gradient(outputs.last(), targets)
+
+        var dErr_dNetIn: Vector = Vector(0)
+
+        ((network.layers.size - 1) downTo 0).forEach { layerIndex ->
+            val layer = network.layers[layerIndex]
+            val new_dErr_dNetIn = MutableVector.zeros(layer.neurons.size)
+
+            layer.neurons.forEachIndexed { neuronIndex, neuron ->
+                val netInput = neuron.activationFunction.calculateNetInput(neuron.weights, inputs[layerIndex])
+                val dErr_dOut: Double =
+                    if (layerIndex == network.layers.size - 1) {
+                        costDeriv[neuronIndex]
+                    } else {
+                        network.layers[layerIndex + 1].neurons.foldIndexed(0.0) { laterNeuronIndex, acc, laterNeuron ->
+                            acc + dErr_dNetIn[laterNeuronIndex] * laterNeuron.weights.coeffs[neuronIndex]
+                        }
+                    }
+                val dOut_dNetIn = neuron.activationFunction.netInputGradient(netInput)
+                new_dErr_dNetIn[neuronIndex] = dErr_dOut * dOut_dNetIn
+
+                if (neuron.weights.hasConstant) {
+                    m[layerIndex][neuronIndex].constant =
+                        betaM * m[layerIndex][neuronIndex].constant +
+                            (1 - betaM) * new_dErr_dNetIn[neuronIndex]
+                    v[layerIndex][neuronIndex].constant =
+                        betaV * v[layerIndex][neuronIndex].constant +
+                            (1 - betaV) * new_dErr_dNetIn[neuronIndex].pow(2)
+                    val mHat = m[layerIndex][neuronIndex].constant / (1 - betaMProduct)
+                    val vHat = v[layerIndex][neuronIndex].constant / (1 - betaVProduct)
+                    neuron.weights.constant -= network.stepSize * mHat / (sqrt(vHat) + epsilon)
+                }
+                inputs[layerIndex].forEachIndexed { coeffIndex, input ->
+                    m[layerIndex][neuronIndex].coeffs[coeffIndex] =
+                        betaM * m[layerIndex][neuronIndex].coeffs[coeffIndex] +
+                            (1 - betaM) * new_dErr_dNetIn[neuronIndex] * input
+                    v[layerIndex][neuronIndex].coeffs[coeffIndex] =
+                        betaV * v[layerIndex][neuronIndex].coeffs[coeffIndex] +
+                            (1 - betaV) * (new_dErr_dNetIn[neuronIndex] * input).pow(2)
+                    val mHat = m[layerIndex][neuronIndex].coeffs[coeffIndex] / (1 - betaMProduct)
+                    val vHat = v[layerIndex][neuronIndex].coeffs[coeffIndex] / (1 - betaVProduct)
+
+                    neuron.weights.coeffs[coeffIndex] -= network.stepSize * mHat / (sqrt(vHat) + epsilon)
+                }
+            }
+
+            dErr_dNetIn = new_dErr_dNetIn
+        }
+    }
+}
--- src/test/kotlin/kotml/regression/optimization/backpropagation/AdamBackpropagationTest.kt
+++ src/test/kotlin/kotml/regression/optimization/backpropagation/AdamBackpropagationTest.kt
@@ -12,19 +12,17 @@ import kotml.regression.cost.loss.HalfSquaredError
 import kotml.regression.cost.loss.SquaredError
 import kotml.regression.functions.LinearFunction
 import kotml.regression.functions.LogisticFunction
-import kotml.regression.functions.ReLU
+import kotml.regression.functions.Tanh
 import kotml.regression.neural.FeedforwardNeuralNetwork
 import kotml.regression.neural.NeuralLayer
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-class StochasticBackpropagationTest {
-    // It's completely unnecessary to use multiple layers of linear
-    // functions, but it's straightforward to debug.
+class AdamBackpropagationTest {
     @Test
     fun `calculates weights correctly with linear functions`() {
         val network = FeedforwardNeuralNetwork(
-            stepSize = 0.001,
+            stepSize = 0.01,
             layers = arrayOf(
                 NeuralLayer(
                     neuronCount = 1,
@@ -44,28 +42,28 @@ class StochasticBackpropagationTest {
             )
         )
 
-        val estimator = StochasticBackpropagation(
+        val estimator = AdamBackpropagation(
             network = network,
             costFunction = SumCost(SquaredError)
         )
 
         val rand = Random(0)
-        (-1000..1000).shuffled(rand).forEach { intX ->
-            val x = intX.toDouble() / 100.0
+        (-700..700).shuffled(rand).forEach { intX ->
+            val x = intX.toDouble() / 70.0
             val target = -1.0 + 2.0 * x
             estimator.observe(Vector(x), Vector(target))
         }
 
-        assertTrue(abs(-1.0 - network.evaluate(Vector(0))[0]) < 0.001)
-        assertTrue(abs(3.0 - network.evaluate(Vector(2))[0]) < 0.001)
-        assertTrue(abs(9.0 - network.evaluate(Vector(5))[0]) < 0.001)
+        assertTrue(abs(-1.0 - network.evaluate(Vector(0))[0]) < 0.01)
+        assertTrue(abs(3.0 - network.evaluate(Vector(2))[0]) < 0.01)
+        assertTrue(abs(9.0 - network.evaluate(Vector(5))[0]) < 0.01)
     }
 
     @Test
-    fun `calculates weights correctly with layers of different functions`() {
+    fun `calculates weights correctly with a constant`() {
         val rand = Random(0)
         val network = FeedforwardNeuralNetwork(
-            stepSize = 0.01,
+            stepSize = 0.0013,
             layers = arrayOf(
                 NeuralLayer(
                     neuronCount = 3,
@@ -74,7 +72,7 @@ class StochasticBackpropagationTest {
                     sampler = NormalSampler(stdev = 2.0, random = rand)),
                 NeuralLayer(
                     neuronCount = 3,
-                    activationFunction = ReLU,
+                    activationFunction = Tanh,
                     regressorCount = 3,
                     sampler = NormalSampler(stdev = 2.0, random = rand)),
                 NeuralLayer(
@@ -85,19 +83,19 @@ class StochasticBackpropagationTest {
             )
         )
 
-        val estimator = StochasticBackpropagation(
+        val estimator = AdamBackpropagation(
             network = network,
             costFunction = MeanCost(HalfSquaredError)
         )
 
         // out1(x1, x2, x3) = 1.5 + x1 + 2 * x2 + 0.5 * x3^2
         // out2(x1, x2, x3) = x1 * x2 + x3
-        (-15..15).shuffled(rand).forEach { intX1 ->
-            (-15..15).shuffled(rand).forEach { intX2 ->
-                (-15..15).shuffled(rand).forEach { intX3 ->
-                    val x1 = intX1.toDouble() / 10.0
-                    val x2 = intX2.toDouble() / 10.0
-                    val x3 = intX3.toDouble() / 10.0
+        (-14..14).shuffled(rand).forEach { intX1 ->
+            (-14..14).shuffled(rand).forEach { intX2 ->
+                (-14..14).shuffled(rand).forEach { intX3 ->
+                    val x1 = intX1.toDouble() / 12.0
+                    val x2 = intX2.toDouble() / 12.0
+                    val x3 = intX3.toDouble() / 12.0
                     val target1 = 1.5 + x1 + 2.0 * x2 + 0.5 * x3.pow(2)
                     val target2 = x1.pow(2) + x2 - x3
                     estimator.observe(Vector(x1, x2, x3), Vector(target1, target2))
@@ -107,6 +105,6 @@ class StochasticBackpropagationTest {
 
         val estimate = network.evaluate(Vector(0.5, 1.5, 1.0))
         assertTrue(abs(5.5 - estimate[0]) < 0.6)
-        assertTrue(abs(0.75 - estimate[1]) < 0.05)
+        assertTrue(abs(0.75 - estimate[1]) < 0.1)
     }
 }

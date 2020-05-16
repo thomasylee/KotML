96b3d5705c9b25d647a0ae6bd5db133d38eefbbd
Thomas Lee <thomas.y.lee@protonmail.com>
2020-05-17 00:00:04 +0100

    Add NeuralLayer.softmax() to create softmax layers

--- src/main/kotlin/kotml/regression/neural/NeuralLayer.kt
+++ src/main/kotlin/kotml/regression/neural/NeuralLayer.kt
@@ -2,17 +2,38 @@ package kotml.regression.neural
 
 import kotml.distributions.DistributionSampler
 import kotml.distributions.NormalSampler
+import kotml.distributions.UniformSampler
 import kotml.math.Vector
 import kotml.regression.RegressionException
 import kotml.regression.functions.FunctionModel
+import kotml.regression.functions.IdentityFunction
 import kotml.regression.functions.aggregation.AggregationFunction
 import kotml.regression.functions.aggregation.DotProduct
+import kotml.regression.functions.aggregation.Softmax
 
 /**
  * `NeuralLayer` contains a collection of neurons to be used in a neural
  * network.
  */
 class NeuralLayer(val neurons: Array<Neuron>) {
+    companion object {
+        fun softmax(neuronCount: Int, regressorCount: Int): NeuralLayer =
+            NeuralLayer(Array<Neuron>(neuronCount) { index ->
+                Neuron(
+                    activationFunction = IdentityFunction,
+                    regressorCount = regressorCount,
+                    includeConstant = false,
+                    sampler = UniformSampler(0.0),
+                    aggregationFunction = Softmax(index)
+                )
+            })
+    }
+
+    init {
+        if (neurons.isEmpty())
+            throw RegressionException("A neural layer cannot have 0 neurons")
+    }
+
     constructor(
         neuronCount: Int,
         activationFunction: FunctionModel,
@@ -29,11 +50,6 @@ class NeuralLayer(val neurons: Array<Neuron>) {
             aggregationFunction
         )
     })
-
-    init {
-        if (neurons.isEmpty())
-            throw RegressionException("A neural layer cannot have 0 neurons")
-    }
 
     /**
      * Evaluates a vector of inputs and returns the results for each neuron.
--- src/test/kotlin/kotml/regression/neural/NeuralLayerTest.kt
+++ src/test/kotlin/kotml/regression/neural/NeuralLayerTest.kt
@@ -2,8 +2,12 @@ package kotml.regression.neural
 
 import kotml.math.Vector
 import kotml.regression.Weights
+import kotml.regression.functions.IdentityFunction
 import kotml.regression.functions.ReLU
+import kotml.regression.functions.aggregation.Softmax
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class NeuralLayerTest {
@@ -16,5 +20,18 @@ class NeuralLayerTest {
             )
         })
         assertEquals(Vector(4, 5, 6), neuralLayer.evaluate(Vector(2)))
+    }
+
+    @Test
+    fun `softmax() returns a softmax neural layer`() {
+        val neuralLayer = NeuralLayer.softmax(5, 3)
+
+        assertEquals(5, neuralLayer.neurons.size)
+        neuralLayer.neurons.forEachIndexed { index, neuron ->
+            assertTrue(neuron.aggregationFunction is Softmax)
+            assertEquals(index, (neuron.aggregationFunction as Softmax).regressorIndex)
+            assertFalse(neuron.weights.hasConstant)
+            assertTrue(neuron.activationFunction is IdentityFunction)
+        }
     }
 }
